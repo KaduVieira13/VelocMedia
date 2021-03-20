9070b70590ce149c29549893b0862b7310e68dd0
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-03-20 16:23:39 -0300

    Velocidade Média finalizado

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int deltaTempo;
+            int deltaDistancia;
+            int velocidade;
+
+           
+            Console.Write("Digite o tempo gasto em segundos..: ");
+            deltaTempo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite a distância percorrida em metros..: ");
+            deltaDistancia = Convert.ToInt32(Console.ReadLine());
+            velocidade = Convert.ToInt32((deltaDistancia) /  (deltaTempo));
+            Console.Write($"Velocidade atingida..: {velocidade} m/s");
+            
+            
+        }
+    }
+}
